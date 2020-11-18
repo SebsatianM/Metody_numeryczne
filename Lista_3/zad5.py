81281bf36e12ae925d7cff540ac44ff67ac52aa8
SebsatianM <sebastiian.mielczarek@op.pl>
2020-11-18 13:35:22 +0100

    Rozwiązanie zadania 5

---
+++ Lista_3/zad5.py
@@ -0,0 +1,12 @@
+import numpy as np
+import scipy.linalg as sc
+
+# Matrix norm
+
+print("Norma dla 5-elementowej macierzy Hilberta:",np.linalg.norm(sc.hilbert(5)))
+print("Norma dla 10-elementowej macierzy Hilberta:",np.linalg.norm(sc.hilbert(10)))
+print("Norma dla 20-elementowej macierzy Hilberta:",np.linalg.norm(sc.hilbert(20)))
+
+print("\nWskaźnik uwarunkowania dla 5-elementowej macierzy Hilberta:",np.linalg.cond(sc.hilbert(5)))
+print("Wskaźnik uwarunkowania dla 10-elementowej macierzy Hilberta:",np.linalg.cond(sc.hilbert(10)))
+print("Wskaźnik uwarunkowania dla 20-elementowej macierzy Hilberta:",np.linalg.cond(sc.hilbert(20)))
