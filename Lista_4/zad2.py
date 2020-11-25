bedfedf3918521c99a9800b219d7b4030d123564
SebsatianM <sebastiian.mielczarek@op.pl>
2020-11-25 13:11:49 +0100

    skończone zadanie 2

--- Lista_4/zad2.py
+++ Lista_4/zad2.py
@@ -1,6 +1,10 @@
 import numpy as np
-import scipy as sc
-def root(x):
-    x0 = a 
+from scipy import optimize
 
+x = int(input("Podaj liczbę z jakiej chcesz obliczyć pierwiastek: "))
+n = int(input("Podaj stopień pierwiaskta który chcesz obliczyć: "))
+y = lambda x,a: x**n-a
+dy = lambda  x,a: n*x**(n-1)
+root = optimize.newton(y, 3, fprime=dy, args=(x,))
 
+print("Pierwiastek %.0f-stopnia z %d jest równy %s" % (n,x,root))
