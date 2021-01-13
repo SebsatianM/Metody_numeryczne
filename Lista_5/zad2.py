f6f55727bdf50c8e50652c12a7f44bb9ed41081a
SebsatianM <sebastiian.mielczarek@op.pl>
2021-01-13 13:43:47 +0100

    dodanie rozwiązań z listy 5

--- Lista_5/zad2.py
+++ Lista_5/zad2.py
@@ -3,23 +3,18 @@ import matplotlib.pyplot as plt
 roots = []
 x = [1,1.25,1.5,1.75,2,2.25,2.5,2.75,3]
 y = [-0.5403,-0.0104,0.9423,1.7445,1.3073,-0.7718,-2.4986,-0.7903,2.7334]
-
-mypolynomial = numpy.poly1d(numpy.polyfit(x, y, 8))
-
+mypolynomial = numpy.poly1d(numpy.polyfit(x, y, 7))
 myline = numpy.linspace(0.95,3, 100)
-
 plt.scatter(x, y)
 plt.plot(myline, mypolynomial(myline))
 val_in_2_1 = mypolynomial(2.1)
 print("Wartości w punkcie 2.1: ", val_in_2_1)
 mypoly2 = mypolynomial.deriv()
 val2_in_2_1 = mypoly2(2.1)
-
 for r in mypolynomial.roots:
     if r >= 1 and r <= 3:
         if not numpy.iscomplex(r):
             roots.append(r)
-
 print("Wartości w punkcie 2.1: przy y' ", val2_in_2_1)
 print("pierwiastki: ",roots)
 plt.show() 
--- Lista_5/zad3.py
+++ Lista_5/zad3.py
@@ -1,8 +1,15 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
+from scipy import interpolate
 Re = [0.2, 2, 20, 200, 2000, 20000]
 Cp = [103, 13.9, 2.72, 0.8, 0.401, 0.433]
 
+log_Re = np.log(Re)
+log_Cp = np.log(Cp)
+Log_CubSpl = interpolate.CubicSpline(log_Re, log_Cp)
+x_to_find = [5, 50, 5000]
+y = np.exp(Log_CubSpl(np.log(x_to_find)))
+print("pierwiastki:", y)
 plt.scatter(Re, Cp)
 plt.yscale('log')
 plt.xscale('log')
