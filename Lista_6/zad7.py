5cf324714ae915a93651161ab102e82c7fe089fd
SebsatianM <sebastiian.mielczarek@op.pl>
2021-01-25 18:51:12 +0100

    dodanie zadań 7 i 8 z listy 6

--- Lista_6/zad7.py
+++ Lista_6/zad7.py
@@ -1,8 +1,15 @@
 import numpy as np
-import scipy as sc
+from scipy.integrate import quadrature as qd 
 
-def f(x):
-    (np.cos(x)-np.exp(x))/np.sin(x)
-    
+fun = lambda x:(np.cos(x)-np.exp(x))/np.sin(x)
+
 X_from = -1
 X_to = 1
+
+Integ1 = qd(fun, X_from, 0, tol=1e-10,maxiter=200)
+
+Integ2 = qd(fun, 0, X_to, tol=1e-10,maxiter=200)
+
+#podział na dwie całki od -1 do 0 i od 0 do 1, ponieważ przy próbie całkowania od -1 do 1 iteracja dążyła do nieskończoności 
+
+print("Wartość całki wynosi: {0}\nBłąd obliczeń wynosi: {1}".format(Integ1[0]+Integ2[0],Integ1[1]+Integ2[1]))
--- Lista_6/zad8.py
+++ Lista_6/zad8.py
@@ -1,3 +1,13 @@
 import numpy as np
-import scipy as sc
+from scipy.integrate import dblquad as dbl
 import math
+
+X_from = 0
+X_to = 1
+
+f = lambda y, x: np.sin(np.pi * y) * np.sin(np.pi * (x - y))
+
+f_lower = lambda x: x
+
+result = dbl(f,X_from,X_to,X_from,f_lower)
+print("Wartość całki wynosi: {0}".format(result[0]))
